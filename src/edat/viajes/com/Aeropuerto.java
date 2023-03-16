f02cccc8a8289fb0735cc64122669885452dfea5
Erick-9827 <erick.carus@est.fi.uncoma.edu.ar>
2023-03-16 15:42:07 -0300

    nuevo

---
+++ src/edat/viajes/com/Aeropuerto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edat.viajes.com;
+
+/**
+ *
+ * @author erick.carus
+ */
+public class Aeropuerto {
+    private String nombreAeronautico;
+    private String ciudad;
+    private int numeroTel;
+    
+}
---
+++ src/edat/viajes/com/Vuelo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edat.viajes.com;
+
+/**
+ *
+ * @author erick.carus
+ */
+public class Vuelo {
+
+    private 
+            private private private private private private 
+            
+            
+            
+            
+            
+            
+            
+    
+}
